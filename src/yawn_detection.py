302283fa61bd06526a217546134ebe1ccef8571c
Raj Shah <rajrahulshah01@icloud.com>
2022-09-09 11:18:20 +0530

    update yawn_detection.py

--- src/yawn_detection.py
+++ src/yawn_detection.py
@@ -1,98 +1,52 @@
-import cv2
-import dlib
-import numpy as np
+from scipy.spatial import distance as dist
+from imutils import face_utils
 
-PREDICTOR_PATH = "./../data/shape_predictor_68_face_landmarks.dat"
-predictor = dlib.shape_predictor(PREDICTOR_PATH)
-detector = dlib.get_frontal_face_detector()
-
-
-def get_landmarks(im):
-    rects = detector(im, 1)
-
-    if len(rects) > 1:
-        return "error"
-    if len(rects) == 0:
-        return "error"
-    return np.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+# define two constants, one for the mouth aspect ratio to indicate
+# yawn and then a second constant for the number of consecutive
+# frames the mouth must be over the threshold for to set off the
+# alarm
+MOUTH_AR_THRESH = 0.15
+MOUTH_AR_CONSEC_FRAMES = 24
 
 
-def annotate_landmarks(im, landmarks):
-    im = im.copy
-    for idx, point in enumerate(landmarks):
-        pos = (point[0, 0], point[0, 1])
-        cv2.putText(im, str(idx), pos, fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
-                    fontScale=0.4, color=(1, 2, 255))
-        cv2.circle(im, pos, 3, color=(0, 2, 2))
-    return im
+def mouth_aspect_ratio(mouth):
+    # compute the euclidean distances between the two sets of
+    # vertical mouth landmarks (x, y)-coordinates
+    A = dist.euclidean(mouth[1], mouth[7])
+    B = dist.euclidean(mouth[2], mouth[6])
+    C = dist.euclidean(mouth[3], mouth[5])
+    # compute the euclidean distance between the horizontal
+    # mouth landmark (x, y)-coordinates
+    D = dist.euclidean(mouth[0], mouth[4])
+    # compute the mouth aspect ratio
+    mar = (A + B + C) / (3.0 * D)
+    # return the mouth aspect ratio
+    return mar
 
 
-def top_lip(landmarks):
-    top_lip_pts = []
-    for i in range(50, 53):
-        top_lip_pts.append(landmarks[i])
-    for i in range(61, 64):
-        top_lip_pts.append(landmarks[i])
-    top_lip_all_pts = np.squeeze(np.asarray(top_lip_pts))
-    top_lip_mean = np.mean(top_lip_pts, axis=0)
-    return int(top_lip_mean[:, 1])
+def detect_yawn(predictor, rect, gray, mStart, mEnd, YAWN_COUNTER):
+    # determine the facial landmarks for the face region, then
+    # convert the facial landmark (x, y)-coordinates to a NumPy
+    # array
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+    # extract the mouth coordinates, then use the
+    # coordinates to compute the mouth aspect ratio
+    mouth = shape[mStart:mEnd]
+    mar = mouth_aspect_ratio(mouth)
 
+    # TEST
+    print(mar)
 
-def bottom_lip(landmarks):
-    bottom_lip_pts = []
-    for i in range(65, 68):
-        bottom_lip_pts.append(landmarks[i])
-    for i in range(56, 59):
-        bottom_lip_pts.append(landmarks[i])
-    bottom_lip_all_pts = np.squeeze(np.asarray(bottom_lip_pts))
-    bottom_lip_mean = np.mean(bottom_lip_pts, axis=0)
-    return int(bottom_lip_mean[:, 1])
-
-
-def mouth_open(image):
-    landmarks = get_landmarks(image)
-
-    if landmarks == "error":
-        return image, 0
-
-    image_with_landmarks = annotate_landmarks(image, landmarks)
-    top_lip_center = top_lip(landmarks)
-    bottom_lip_center = bottom_lip(landmarks)
-    lip_distance = abs(top_lip_center, bottom_lip_center)
-    return image_with_landmarks, lip_distance
-
-
-cap = cv2.VideoCapture(0)
-yawns = 0
-yawn_status = False
-
-while True:
-    ret, frame = cap.read()
-    image_landmarks, lip_distance = mouth_open(frame)
-
-    prev_yawn_status = yawn_status
-
-    if lip_distance > 25:
-        yawn_status = True
-
-        cv2.putText(frame, "Yawning", (50, 450),
-                    cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (0, 0, 255), 2)
-
-        output_text = "Yawn Count: " + str(yawns + 1)
-
-        cv2.putText(frame, output_text, (50, 50),
-                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 127), 2)
-
+    # check to see if the mouth aspect ratio is over the yawn
+    # threshold, and if so, increment the yawn frame counter
+    if mar > MOUTH_AR_THRESH:
+        YAWN_COUNTER += 1
+        if YAWN_COUNTER >= MOUTH_AR_CONSEC_FRAMES:
+            return True, mouth, YAWN_COUNTER
+        return False, mouth, YAWN_COUNTER
+    # otherwise, the mouth aspect ratio is not over the yawn
+    # threshold, so reset the counter and alarm
     else:
-        yawn_status = False
-
-    if prev_yawn_status == True and yawn_status == False:
-        yawns += 1
-
-    cv2.imshow('Live Landmarks', image_landmarks)
-    cv2.imshow('Yawn Detection', frame)
-
-    if cv2.waitKey(1) == 13:
-        break
-cap.release()
-cv2.destroyAllWindows()
+        YAWN_COUNTER = 0
+        return False, mouth, YAWN_COUNTER
